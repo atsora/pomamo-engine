4773e6ba0efaab108fe7cee5a24fe47cfbc31bfe
Nicolas Relange <miredo@relange.org>
2023-06-30 22:59:44 +0200

    Lem_RestartService: improve the logs (above all those with an exception)

--- Services/Lem_RestartService/Program.cs
+++ Services/Lem_RestartService/Program.cs
@@ -90,10 +90,7 @@ namespace Lem_RestartService
       } catch (Exception ex) {
         string errorMessage = "The service " + serviceName + " could not be stopped. " +
           "Did you run the program with the Administrator privilege ?";
-        log.ErrorFormat ("RestartService: " +
-                         "the service could not be stopped because of {0}, " +
-                         "error message={1}",
-                         ex, errorMessage);
+        log.Error ($"RestartService: the service {serviceName} could not be stopped", ex);
         System.Console.WriteLine (errorMessage);
       }
       
@@ -104,20 +101,14 @@ namespace Lem_RestartService
         pid = GetServicePID (serviceName);
       }
       catch (Exception ex) {
-        log.ErrorFormat ("RestartService: " +
-                         "GetServicePID raised {0}",
-                         ex);
+        log.Error ($"RestartService: exception in GetServicePID", ex);
       }
       if (0 == pid) {
-        log.DebugFormat ("RestartService: " +
-                         "no process for {0}",
-                         serviceName);
+        log.Debug ($"RestartService: no process for {serviceName}");
       }
       else {
         if (!KillProcess (pid)) {
-          log.ErrorFormat ("RestartService: " +
-                           "process with pid={0} could not be stopped",
-                           pid);
+          log.Error ($"RestartService: process with pid={pid} could not be stopped");
         }
         Thread.Sleep (TimeSpan.FromSeconds (1));
       }
@@ -134,10 +125,7 @@ namespace Lem_RestartService
       } catch (Exception ex) {
         string errorMessage = "The service " + serviceName + " could not be started. " +
           "Did you run the program with the Administrator privilege ?";
-        log.ErrorFormat ("RestartService: " +
-                         "the service could not be started because of {0}, " +
-                         "error message={1}",
-                         ex, errorMessage);
+        log.Error ($"RestartService: the service {serviceName} could not be started", ex);
         System.Console.WriteLine (errorMessage);
       }
     }
@@ -156,12 +144,7 @@ namespace Lem_RestartService
         status = controlSC.Status;
       }
       catch (Exception ex) {
-        log.WarnFormat ("StopWatchingService: " +
-                        "could not get the status of the service, " +
-                        "it is probably not installed ! " +
-                        "=> give up (service not running) " +
-                        "{0}",
-                        ex);
+        log.Warn ($"StopWatchingService: could not get the status of the service, it is probably not installed ! => give up (service not running)", ex);
         return false;
       }
       
@@ -172,8 +155,7 @@ namespace Lem_RestartService
           return false;
         }
         else { // The service was running
-          log.DebugFormat ("StopWatchingService: " +
-                           "the watching service was running");
+          log.Debug ($"StopWatchingService: the watching service was running");
           
           // - Stop the service
           controlSC.Stop();
@@ -188,9 +170,7 @@ namespace Lem_RestartService
             pid = GetServicePID (controlSC.ServiceName);
           }
           catch (Exception ex) {
-            log.ErrorFormat ("StopWatchingService: " +
-                             "GetServicePID raised {0}",
-                             ex);
+            log.Error ($"StopWatchingService: exception in GetServicePID", ex);
           }
           if (0 == pid) {
             log.DebugFormat ("StopWatchingService: " +
@@ -208,10 +188,7 @@ namespace Lem_RestartService
         }
       }
       catch (Exception ex) {
-        log.WarnFormat ("StopWatchingService: " +
-                        "the watching service could not be stopped because of " +
-                        "{0}",
-                        ex);
+        log.Warn ($"StopWatchingService: the watching service could not be stopped", ex);
       }
       
       return true;
@@ -237,11 +214,7 @@ namespace Lem_RestartService
         }
       }
       catch (Exception ex) {
-        log.ErrorFormat ("StartWatchingService: " +
-                         "the watching service is probably not installed, " +
-                         "=> give up " +
-                         "{0}",
-                         ex);
+        log.Error ($"StartWatchingService: the watching service is probably not installed, => give up", ex);
         return;
       }
     }
@@ -259,29 +232,21 @@ namespace Lem_RestartService
         process = Process.GetProcessById (pid);
       }
       catch (Exception ex) {
-        log.DebugFormat ("KillProcess: " +
-                         "process with pid {0} can't be found " +
-                         "=> it probably does not exist any more, return true " +
-                         "{0}",
-                         ex);
+        log.Debug ($"KillProcess: process with pid {pid} can't be found => it probably does not exist any more, return true", ex);
         return true;
       }
       
       // 1st attempt: try to stop first properly the process
       try {
-        log.Info ("KillProcess: " +
-                  "Stop the process");
+        log.Info ("KillProcess: Stop the process");
         process.EnableRaisingEvents = true;
         if (false == process.CloseMainWindow ()) {
-          log.Debug ("KillProcess: " +
-                     "the close message could not be sent");
+          log.Debug ("KillProcess: the close message could not be sent");
         }
         process.Close ();
       }
       catch (Exception ex) {
-        log.ErrorFormat ("KillProcess: " +
-                         "CloseMainWindow or Close returned exception {0}",
-                         ex);
+        log.Error ("KillProcess: CloseMainWindow or Close returned exception", ex);
       }
       
       // Check if the process was stopped after maximum 5 seconds
@@ -289,41 +254,26 @@ namespace Lem_RestartService
         process = Process.GetProcessById (pid);
       }
       catch (Exception ex) {
-        log.DebugFormat ("KillProcess: " +
-                         "process with pid {0} can't be found " +
-                         "=> it probably does not exist any more, return true " +
-                         "{0}",
-                         ex);
+        log.Debug ($"KillProcess: process with pid {pid} can't be found => it probably does not exist any more, return true", ex);
         return true;
       }
       try {
         if (!process.WaitForExit (5000)) {
-          log.Info ("Process is still running " +
-                    "after 5s !");
+          log.Info ("Process is still running after 5s !");
           // Kill the process
         }
         else {
-          log.InfoFormat ("KillProcess: " +
-                          "Nice, the process " +
-                          "does not exist any more");
+          log.Info ("KillProcess: Nice, the process does not exist any more");
           return true;
         }
       }
       catch (Exception ex1) {
-        log.InfoFormat ("KillProcess: " +
-                        "WaitForExit returned {0} " +
-                        "check if the process still exists",
-                        ex1);
+        log.Info ($"KillProcess: WaitForExit returned an exception check if the process still exists", ex1);
         try {
           process = Process.GetProcessById (pid);
         }
         catch (Exception ex2) {
-          log.InfoFormat ("KillProcess: " +
-                          "process pid={0} does not exist any more, " +
-                          "=> return true " +
-                          "{1}",
-                          pid,
-                          ex2);
+          log.Info ($"KillProcess: process pid={pid} does not exist any more, => return true", ex2);
           return true;
         }
       }
@@ -333,80 +283,47 @@ namespace Lem_RestartService
         process = Process.GetProcessById (pid);
       }
       catch (Exception ex) {
-        log.DebugFormat ("KillProcess: " +
-                         "process with pid {0} can't be found " +
-                         "=> it probably does not exist any more, return true " +
-                         "{0}",
-                         ex);
+        log.Debug ($"KillProcess: process with pid {pid} can't be found => it probably does not exist any more, return true", ex);
         return true;
       }
       try {
-        log.Debug ("KillProcess: " +
-                   "Kill the process");
+        log.Debug ("KillProcess: Kill the process");
         process.Kill ();
         log.Info ("The process was successfully killed");
         return true;
       }
       catch (System.ComponentModel.Win32Exception ex) {
-        log.InfoFormat ("KillProcess: " +
-                        "The process could not be terminated " +
-                        "or is terminating " +
-                        "=> wait 10 s, else give up " +
-                        "{0}",
-                        ex);
+        log.Info ("KillProcess: The process could not be terminated or is terminating => wait 10 s, else give up", ex);
         try {
           if (!process.WaitForExit (10000)) {
-            log.ErrorFormat ("KillProcess: " +
-                             "Process is still running " +
-                             "after 10s ! " +
-                             "{0}",
-                             ex);
+            log.Error ($"KillProcess: Process is still running after 10s !", ex);
             return false;
           }
           else {
-            log.InfoFormat ("KillProcess: " +
-                            "Nice, the process " +
-                            "does not exist any more");
+            log.Info ("KillProcess: Nice, the process does not exist any more");
             return true;
           }
         }
         catch (Exception ex1) {
-          log.InfoFormat ("KillProcess: " +
-                          "WaitForExit returned {0} " +
-                          "check if the process still exists",
-                          ex1);
+          log.Info ("KillProcess: WaitForExit returned an exception check if the process still exists", ex1);
           try {
             process = Process.GetProcessById (process.Id);
           }
-          catch (Exception) {
-            log.InfoFormat ("KillProcess: " +
-                            "process pid={0} does not exist any more, " +
-                            "=> return true",
-                            process.Id);
+          catch (Exception ex2) {
+            log.Info ($"KillProcess: process pid={process.Id} does not exist any more, => return true", ex2);
             return true;
           }
-          log.ErrorFormat ("KillProcess: " +
-                           "the process pid={0} could not be killed, " +
-                           "may be because the used privilege was not high enough",
-                           process.Id);
+          log.Error ($"KillProcess: the process pid={process.Id} could not be killed, may be because the used privilege was not high enough", ex1);
           return false;
         }
       }
       catch (NotSupportedException ex) {
-        log.FatalFormat ("KillProcess: " +
-                         "The process is a local process " +
-                         "this exception should not happen " +
-                         "{0}",
-                         ex);
+        log.Fatal ($"KillProcess: The process is a local process this exception should not happen", ex);
         Debug.Assert (false);
         return false;
       }
       catch (InvalidOperationException ex) {
-        log.WarnFormat ("KillProcess: " +
-                        "The process is invalid " +
-                        "=> it can't be killed " +
-                        "{0}",
-                        ex);
+        log.Warn ($"KillProcess: The process is invalid => it can't be killed", ex);
         return true;
       }
     }
@@ -425,19 +342,13 @@ namespace Lem_RestartService
       RegistryKey registryKey = Registry.LocalMachine
         .OpenSubKey("SYSTEM\\CurrentControlSet\\Services\\"+serviceName, false);
       if (null == registryKey) {
-        log.ErrorFormat ("GetServicePID: " +
-                         "the registry key of the service {0} does not exist",
-                         serviceName);
+        log.Error ($"GetServicePID: the registry key of the service {serviceName} does not exist");
         throw new ArgumentException ("service does not exist");
       }
       else { // null != rk
         object serviceImagePath = registryKey.GetValue("ImagePath");
         if (null == serviceImagePath) {
-          log.ErrorFormat ("GetServicePID: " +
-                           "the registry key for service {0} does not have the value ImagePath " +
-                           "The registry key is {1}",
-                           serviceName,
-                           registryKey);
+          log.Error ($"GetServicePID: the registry key for service {serviceName} does not have the value ImagePath The registry key is {registryKey}");
           throw new ApplicationException ("No ImagePath");
         }
         else { // null != serviceImagePath
@@ -454,18 +365,12 @@ namespace Lem_RestartService
             path = pathAndParameters[0];
           }
           
-          log.DebugFormat ("GetServicePID: " +
-                           "Got Path for Service {0} = {1}",
-                           serviceName, path);
+          log.Debug ($"GetServicePID: got Path for Service {serviceName} = {path}");
           string exe = Path.GetFileNameWithoutExtension (path);
           System.Diagnostics.Process[] processes =
             System.Diagnostics.Process.GetProcessesByName(exe);
           if (processes.Length <= 0)  {
-            log.DebugFormat ("GetServicePID: " +
-                             "there is no process with the name {0}, " +
-                             "may be it is stopped " +
-                             "=> return 0",
-                             exe);
+            log.Debug ($"GetServicePID: there is no process with the name {exe}, may be it is stopped => return 0");
             return 0;
           }
           else { // 0 < processes.Length
@@ -475,7 +380,7 @@ namespace Lem_RestartService
                               processes.Length, exe);
             }
             log.DebugFormat ("GetServicePID: " +
-                             "first PID is {0} for servie {1}",
+                             "first PID is {0} for service {1}",
                              processes [0].Id, serviceName);
             return processes [0].Id;
           }
