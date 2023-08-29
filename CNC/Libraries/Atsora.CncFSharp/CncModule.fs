5e4925874f1723ff0d81abe38a73f6fbfb6448c3
Nicolas Relange <miredo@relange.org>
2023-08-29 15:04:23 +0200

    Rewrite some cnc modules: compilation

---
+++ CNC/Libraries/Atsora.CncFSharp/CncModule.fs
@@ -0,0 +1,32 @@
+﻿module Atsora.CncFSharp
+
+open Lemoine.Core.Log
+open Lemoine.Threading
+
+type CncModule(name) =
+  let mutable cncAcquisitionId = 0 in
+  let mutable logger = LogManager.GetLogger (name: string)
+  let mutable dataHandler: IChecked = null
+
+  member val Logger = logger
+
+  member this.CncAcquisitionId
+    with get () =
+      cncAcquisitionId
+    and set id =
+      logger <- LogManager.GetLogger (sprintf "%s.%d" name id)
+      cncAcquisitionId <- id
+
+  member val CncAcquisitionName = "" with get, set
+
+  member this.SetDataHandler dh =
+    dataHandler <- dh
+
+  member this.PauseCheck() =
+    if dataHandler <> null then dataHandler.PauseCheck ()
+
+  member this.ResumeCheck() =
+    if dataHandler <> null then dataHandler.ResumeCheck ()
+
+  member this.SetActive() = 
+    if dataHandler <> null then dataHandler.SetActive ()
