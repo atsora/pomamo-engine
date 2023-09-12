1a684c925537a939c3cdea3e69b8b9471d2fd37a
Nicolas Relange <miredo@relange.org>
2023-09-12 11:59:23 +0200

    Rewrite some cnc modules: DataQueue (initial implementation and xml configuration update)

--- CNC/Libraries/Atsora.CncFSharp/CncModule.fs
+++ CNC/Libraries/Atsora.CncFSharp/CncModule.fs
@@ -10,12 +10,23 @@ type CncModule(name) =
 
   member val Logger = logger
 
+  member val BaseLogCategory = name
+
+  abstract member GetLogCategory: unit -> string
+  default this.GetLogCategory () =
+    sprintf "%s.%d" this.BaseLogCategory cncAcquisitionId
+
+  abstract member UpdateLogger: unit -> unit
+  default this.UpdateLogger () =
+    let s = this.GetLogCategory () in
+    logger <- LogManager.GetLogger s
+
   member this.CncAcquisitionId
     with get () =
       cncAcquisitionId
     and set id =
-      logger <- LogManager.GetLogger (sprintf "%s.%d" name id)
       cncAcquisitionId <- id
+      this.UpdateLogger ()
 
   member val CncAcquisitionName = "" with get, set
 
