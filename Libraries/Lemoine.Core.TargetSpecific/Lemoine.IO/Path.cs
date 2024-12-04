ade132e038f2f7ef175fb6b5536a750c16dfe1ed
Nicolas Relange <miredo@relange.org>
2024-12-04 16:54:41 +0100

    Stamper: fix a bug when the input file and the output file are the same

---
+++ Libraries/Lemoine.Core.TargetSpecific/Lemoine.IO/Path.cs
@@ -0,0 +1,59 @@
+﻿// Copyright (C) 2024 Atsora Solutions
+//
+// SPDX-License-Identifier: Apache-2.0
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lemoine.IO
+{
+  /// <summary>
+  /// Extension to System.IO.Path
+  /// </summary>
+  public class Path
+  {
+    /// <summary>
+    /// Check if two paths are equal
+    /// </summary>
+    /// <param name="path1"></param>
+    /// <param name="path2"></param>
+    /// <returns></returns>
+    public static bool IsSame (string path1, string path2)
+    {
+      var p1 = System.IO.Path.GetFullPath (path1);
+      var p2 = System.IO.Path.GetFullPath (path2);
+      var caseSensitive = IsCaseSensitive ();
+      return string.Equals (p1, p2, caseSensitive ? StringComparison.InvariantCulture : StringComparison.InvariantCultureIgnoreCase);
+    }
+
+    /// <summary>
+    /// Check if a path is case sensitive
+    /// </summary>
+    /// <returns></returns>
+    public static bool IsCaseSensitive ()
+    {
+#if NET48_OR_GREATER || NETCOREAPP
+      if (RuntimeInformation.IsOSPlatform (OSPlatform.Windows)
+        || RuntimeInformation.IsOSPlatform (OSPlatform.OSX)) { // HFS+ (the Mac file-system) is usually configured to be case insensitive.
+        return false;
+      }
+      else if (RuntimeInformation.IsOSPlatform (OSPlatform.Linux)) {
+        return true;
+      }
+      else if (Environment.OSVersion.Platform == PlatformID.Unix) {
+        return true;
+      }
+      else {
+        // A default.
+        return false;
+      }
+#else // not NET48 or NETCOREAPP
+      return false;
+#endif
+    }
+  }
+}
---
+++ UnitTests/Lemoine.Core.UnitTests/IO/Path_UnitTest.cs
@@ -0,0 +1,49 @@
+﻿// Copyright (C) 2024 Atsora Solutions
+//
+// SPDX-License-Identifier: Apache-2.0
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Lemoine.Core.Log;
+using Lemoine.IO;
+
+using NUnit.Framework;
+
+namespace Lemoine.Core.UnitTests.IO
+{
+  /// <summary>
+  /// Test <see cref="Lemoine.IO.Path"/>
+  /// </summary>
+  public class Path_UnitTest
+  {
+    readonly ILog log = LogManager.GetLogger (typeof (Path_UnitTest).FullName);
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    public Path_UnitTest ()
+    { }
+
+    /// <summary>
+    /// Test IsCaseSensitive
+    /// </summary>
+    [Test]
+    public void TestIsCaseSensitive ()
+    {
+      Assert.That (Lemoine.IO.Path.IsCaseSensitive (), Is.False);
+    }
+
+    /// <summary>
+    /// Test IsSame
+    /// </summary>
+    [Test]
+    public void TestIsSame ()
+    {
+      Assert.That (Lemoine.IO.Path.IsSame ("C:\\Test", "C:\\test"), Is.True);
+      Assert.That (Lemoine.IO.Path.IsSame ("C:\\Test", "C:\\Test2"), Is.False);
+    }
+  }
+}
