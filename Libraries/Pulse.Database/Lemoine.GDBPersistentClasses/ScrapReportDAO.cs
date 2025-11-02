4b56ea087d36d8fa01756d688b76e528b4935725
Nicolas Relange <nicolas@atsora.eu>
2025-11-02 14:55:34 +0100

    Web: Scrap at service

--- Libraries/Pulse.Database/Lemoine.GDBPersistentClasses/ScrapReportDAO.cs
+++ Libraries/Pulse.Database/Lemoine.GDBPersistentClasses/ScrapReportDAO.cs
@@ -2,11 +2,18 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-using System;
+using Lemoine.Core.Log;
+using Lemoine.Database.Persistent;
 using Lemoine.Model;
 using Lemoine.ModelDAO;
-using Lemoine.Core.Log;
 using NHibernate;
+using NHibernate.Criterion;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Lemoine.GDBPersistentClasses
 {
@@ -19,5 +26,163 @@ namespace Lemoine.GDBPersistentClasses
   {
     static readonly ILog log = LogManager.GetLogger (typeof (ScrapReportDAO).FullName);
 
+    /// <summary>
+    /// Get a projection of the lower bound of the date/time range
+    /// </summary>
+    /// <returns></returns>
+    protected virtual IProjection GetLowerProjection ()
+    {
+      return Projections
+        .SqlFunction ("lower",
+          new NHibernateTypes.UTCDateTimeFullType (),
+          Projections.Property ("DateTimeRange"));
+    }
+
+    /// <summary>
+    /// Get a projection of the upper bound of the date/time range
+    /// </summary>
+    /// <returns></returns>
+    protected virtual IProjection GetUpperProjection ()
+    {
+      return Projections
+        .SqlFunction ("upper",
+          new NHibernateTypes.UTCDateTimeFullType (),
+          Projections.Property ("DateTimeRange"));
+    }
+
+    /// <summary>
+    /// Range criterion with UTC date/times using directly the range
+    /// </summary>
+    /// <param name="range"></param>
+    /// <returns></returns>
+    protected virtual AbstractCriterion OverlapRange (UtcDateTimeRange range)
+    {
+      return new SimpleExpression ("DateTimeRange", range, "&&");
+    }
+
+    /// <summary>
+    /// <see cref="IScrapReportDAO"/>
+    /// </summary>
+    /// <param name="machine"></param>
+    /// <param name="at"></param>
+    /// <returns></returns>
+    public async Task<IScrapReport> FindActiveAtAsync (IMachine machine, DateTime at)
+    {
+      DateTime utc;
+      switch (at.Kind) {
+        case DateTimeKind.Utc:
+          utc = at;
+          break;
+        case DateTimeKind.Local:
+          utc = at.ToUniversalTime ();
+          break;
+        case DateTimeKind.Unspecified:
+        default:
+          log.Error ($"FindActiveAtAsync: date/time {at} is of kind Unspecified");
+          Debug.Assert (DateTimeKind.Unspecified != at.Kind);
+          utc = at;
+          break;
+      }
+      var scrapReports = await NHibernateHelper.GetCurrentSession ()
+        .CreateCriteria<ScrapReport> ()
+        .Add (Restrictions.Eq ("Machine.Id", machine.Id))
+        // Note: new SimpleExpression ("DateTimeRange", dateTime, "@>") does not work because it compares object of different types
+        // The returned error is: Type mismatch in NHibernate.Criterion.SimpleExpression: DateTimeRange expected type Lemoine.Model.UtcDateTimeRange, actual type System.DateTime
+        .Add (new SimpleTypedExpression ("DateTimeRange", new Lemoine.NHibernateTypes.UTCDateTimeFullType (), utc, "@>"))
+        .ListAsync<IScrapReport> ();
+      if (0 == scrapReports.Count) {
+        return null;
+      }
+      else if (1 == scrapReports.Count) {
+        return scrapReports.Single ();
+      }
+      else {
+        return scrapReports.Single (x => !scrapReports.Any (y => (y.ReportUpdate?.Id ?? 0) == x.Id));
+      }
+    }
+
+    public async Task<IEnumerable<IScrapReport>> FindOverlapsRange (IMachine machine, UtcDateTimeRange range)
+    {
+      var scrapReports = await NHibernateHelper.GetCurrentSession ()
+        .CreateCriteria<ScrapReport> ()
+        .Add (Restrictions.Eq ("Machine.Id", machine.Id))
+        .Add (OverlapRange (range))
+        .ListAsync<IScrapReport> ();
+      return scrapReports.Where (x => !scrapReports.Any (y => (y.ReportUpdate?.Id ?? 0) == x.Id));
+    }
+
+    /// <summary>
+    /// <see cref="IScrapReportDAO"/>
+    /// </summary>
+    public async Task<DateTime?> FindLatestReportTimeBeforeAsync (IMachine machine, IOperationSlot operationSlot, DateTime before)
+    {
+      DateTime utcBefore;
+      switch (before.Kind) {
+        case DateTimeKind.Utc:
+          utcBefore = before;
+          break;
+        case DateTimeKind.Local:
+          utcBefore = before.ToUniversalTime ();
+          break;
+        case DateTimeKind.Unspecified:
+        default:
+          log.Error ($"FindLatestReportTimeBeforeAsync: date/time {before} is of kind Unspecified");
+          Debug.Assert (DateTimeKind.Unspecified != before.Kind);
+          utcBefore = before;
+          break;
+      }
+      var upperProjection = GetUpperProjection ();
+      var scrapReport = await NHibernateHelper.GetCurrentSession ()
+        .CreateCriteria<ScrapReport> ()
+        .Add (Restrictions.Eq ("Machine.Id", machine.Id))
+        .Add (OverlapRange (operationSlot.DateTimeRange))
+        .Add (Restrictions.Lt (upperProjection, utcBefore))
+        .AddOrder (Order.Desc (upperProjection))
+        .SetMaxResults (1)
+        .UniqueResultAsync<IScrapReport> ();
+      if (scrapReport is null) {
+        return null;
+      }
+      else {
+        return scrapReport.DateTimeRange.Lower.Value;
+      }
+    }
+
+    /// <summary>
+    /// <see cref="IScrapReportDAO"/>
+    /// </summary>
+    public async Task<DateTime?> FindNextReportTimeAfterAsync (IMachine machine, IOperationSlot operationSlot, DateTime after)
+    {
+      DateTime utcAfter;
+      switch (after.Kind) {
+        case DateTimeKind.Utc:
+          utcAfter = after;
+          break;
+        case DateTimeKind.Local:
+          utcAfter = after.ToUniversalTime ();
+          break;
+        case DateTimeKind.Unspecified:
+        default:
+          log.Error ($"FindNextReportTimeAfterAsync: date/time {after} is of kind Unspecified");
+          Debug.Assert (DateTimeKind.Unspecified != after.Kind);
+          utcAfter = after;
+          break;
+      }
+      var lowerProjection = GetLowerProjection ();
+      var scrapReport = await NHibernateHelper.GetCurrentSession ()
+        .CreateCriteria<ScrapReport> ()
+        .Add (Restrictions.Eq ("Machine.Id", machine.Id))
+        .Add (OverlapRange (operationSlot.DateTimeRange))
+        .Add (Restrictions.Ge (lowerProjection, utcAfter))
+        .AddOrder (Order.Asc (lowerProjection))
+        .SetMaxResults (1)
+        .UniqueResultAsync<IScrapReport> ();
+      if (scrapReport is null) {
+        return null;
+      }
+      else {
+        return scrapReport.DateTimeRange.Lower.Value;
+      }
+    }
   }
 }
---
+++ Web/Libraries/Pulse.Web/Scrap/ScrapAtRequestDTO.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (C) 2025 Atsora Solutions
+//
+// SPDX-License-Identifier: Apache-2.0
+
+using System;
+
+using System.Collections.Generic;
+using System.Net;
+using Lemoine.Extensions.Web.Attributes;
+using Lemoine.Extensions.Web.Interfaces;
+using Lemoine.Core.Log;
+using Pulse.Web.CommonResponseDTO;
+
+namespace Pulse.Web.Scrap
+{
+  /// <summary>
+  /// Request DTO
+  /// </summary>
+  [Api ("Request DTO for /Scrap/At service")]
+  [ApiResponse (HttpStatusCode.InternalServerError, "Oops, something broke")]
+  [Route ("/Scrap/At/", "GET", Summary = "Get the scrap status at the specified UTC date/time (default is now)", Notes = "To use with ?MachineId=&At=")]
+  [Route ("/Scrap/At/Get", "GET", Summary = "Get the scrap status at the specified UTC date/time (default is now)", Notes = "To use with ?MachineId=&At=")]
+  [Route ("/Scrap/At/Get/{MachineId}/{At}", "GET", Summary = "Get the scrap status at the specified UTC date/time (default is now)", Notes = "")]
+  [Route ("/ScrapAt/", "GET", Summary = "Get the scrap status at the specified UTC date/time (default is now)", Notes = "To use with ?MachineId=&At=")]
+  public class ScrapAtRequestDTO : IReturn<ScrapAtResponseDTO>
+  {
+    /// <summary>
+    /// Id of the monitored machine
+    /// </summary>
+    [ApiMember (Name = "MachineId", Description = "Machine Id", ParameterType = "path", DataType = "int", IsRequired = true)]
+    public int MachineId { get; set; }
+
+    /// <summary>
+    /// UTC date/time of the request
+    /// </summary>
+    [ApiMember (Name = "At", Description = "UTC date/time of the request in ISO format", ParameterType = "path", DataType = "string")]
+    public string At { get; set; }
+  }
+}
