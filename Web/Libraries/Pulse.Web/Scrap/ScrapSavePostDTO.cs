49c607188a99fb7061c30213f8ebe692ac7e0ffb
Nicolas Relange <nicolas@atsora.eu>
2025-11-04 13:04:29 +0100

    Web / Scrap: Save request

--- Web/Libraries/Pulse.Web/Scrap/ScrapSavePostDTO.cs
+++ Web/Libraries/Pulse.Web/Scrap/ScrapSavePostDTO.cs
@@ -10,23 +10,44 @@ using Lemoine.Extensions.Web.Attributes;
 using Lemoine.Extensions.Web.Interfaces;
 using Lemoine.Core.Log;
 using System.Text.Json;
+using System.ComponentModel;
+using Lemoine.Web.CommonRequestDTO;
 
 namespace Pulse.Web.Scrap
 {
   /// <summary>
+  /// Reason part of <see cref="ScrapSavePostDTO"/>
+  /// </summary>
+  public class ScrapSaveReasonPostDTO
+  {
+    [ApiMember (Name = "Id", Description = "Non conformance reason id", ParameterType = "post", DataType = "int", IsRequired = true)]
+    public int Id { get; set; }
+
+    [ApiMember (Name = "Number", Description = "Number of associated parts", ParameterType = "post", DataType = "int", IsRequired = true)]
+    public int Number { get; set; }
+  }
+
+  /// <summary>
   /// Post request DTO for ReasonSave
   /// </summary>
-  public class ScrapSavePostDTO : Lemoine.Web.CommonRequestDTO.RangesPostDTO
+  public class ScrapSavePostDTO : PostDTO
   {
+    [ApiMember (Name = "Id", Description = "Id of a previous scrap report (Default is 0, not applicable)", ParameterType = "post", DataType = "long", IsRequired = false)]
+    [DefaultValue (0)]
+    public long Id { get; set; } = 0;
+
     [ApiMember (Name = "Range", Description = "Date/time range", ParameterType = "post", DataType = "String", IsRequired = true)]
     public string Range { get; set; }
 
-    // TODO: update scrapreport
+    [ApiMember (Name = "ValidCount", Description = "Number of valid parts", ParameterType = "post", DataType = "int", IsRequired = false)]
+    [DefaultValue (0)]
+    public int ValidCount { get; set; } = 0;
 
-    /// <summary>
-    /// Reason data
-    /// </summary>
-    [ApiMember (Name = "ReasonData", Description = "Reason data", ParameterType = "post", DataType = "JsonElement", IsRequired = false)]
-    public JsonElement? ReasonData { get; set; }
+    [ApiMember (Name = "Details", Description = "Additional comment", ParameterType = "post", DataType = "String", IsRequired = false)]
+    [DefaultValue ("")]
+    public string Details { get; set; } = "";
+
+    [ApiMember (Name = "Reasons", Description = "Quantity per non conformance reason", ParameterType = "post", DataType = "list", IsRequired = false)]
+    public IList<ScrapSaveReasonPostDTO> Reasons { get; set; } = new List<ScrapSaveReasonPostDTO> ();
   }
 }
