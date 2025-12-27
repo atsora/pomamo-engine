b9c17ad5fa6810cff91ad56ed83437faf03cf9cd
Nicolas Relange <nicolas@atsora.eu>
2025-12-27 16:33:05 +0100

    Web: NoDetails config in ReasonSelection table

--- Libraries/Pulse.Extensions.Database/IReasonExtension.cs
+++ Libraries/Pulse.Extensions.Database/IReasonExtension.cs
@@ -225,7 +225,10 @@ namespace Pulse.Extensions.Database
     /// <summary>
     /// <see cref="IReasonSelection"/>
     /// </summary>
-    public bool NoDetails => false;
+    public bool NoDetails
+    {
+      get; set;
+    } = false;
 
     /// <summary>
     /// <see cref="IReasonSelection"/>
@@ -412,7 +415,11 @@ namespace Pulse.Extensions.Database
     /// <summary>
     /// <see cref="IReasonSelection"/>
     /// </summary>
-    public bool NoDetails => true;
+    public bool NoDetails
+    {
+      get => true;
+      set => throw new InvalidOperationException ();
+    }
 
     /// <summary>
     /// Id. Not used, always 0
