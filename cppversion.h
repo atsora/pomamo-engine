599ac758b8a2092e22f73cf7fde884ff313cd46f
Nicolas Relange <nicolas@atsora.eu>
2026-03-09 17:58:16 +0100

    18.12.0

--- cppversion.h
+++ cppversion.h
@@ -2,7 +2,7 @@
 #define STRINGIZE(s) STRINGIZE2(s)
  
 #define VERSION_MAJOR 18
-#define VERSION_MINOR 11
+#define VERSION_MINOR 12
 #define VERSION_REVISION 0
 #define VERSION_BUILD               0
 
