c95aa79370ded772b709be00edba20d07e6f7911
Nicolas Relange <nicolas@atsora.eu>
2026-09-25 19:26:08 +0200

    BWIndustrie: signal package Optimize SignalCncAlarmSeverity Fix compilation

--- Libraries/Pulse.Business/CncAlarm/CncAlarmSeverityOption.cs
+++ Libraries/Pulse.Business/CncAlarm/CncAlarmSeverityOption.cs
@@ -2,14 +2,17 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-namespace Pulse.Web.CncAlarm
+namespace Lemoine.Business.CncAlarm
 {
   /// <summary>
   /// Option to get the severity of the cnc alarms and current cnc alarms
-  /// from the cached business request Lemoine.Business.CncAlarm.CncAlarmSeverityFromAttributes
+  /// from the cached business request <see cref="CncAlarmSeverityFromAttributes"/>
   /// instead of from the costly dynamic column cncalarmseverityid
+  ///
+  /// It is used by the web services CncAlarm/At, CncAlarm/Current, CncAlarm/Color
+  /// and by the plugin SignalCncAlarmSeverity
   /// </summary>
-  internal static class CncAlarmSeverityOption
+  public static class CncAlarmSeverityOption
   {
     static readonly string BUSINESS_SEVERITY_KEY = "Web.CncAlarm.BusinessSeverity";
     static readonly bool BUSINESS_SEVERITY_DEFAULT = true;
